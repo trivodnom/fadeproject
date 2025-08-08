1c73840373a94f7d2e86c0b8a4e40b1b2d3a03e3
Aleksey <trivodnom.a@gmail.com>
2025-08-08 15:45:30 +0000

    Add telegram bot v1.2.0

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta # <-- ДОБАВЛЕН ИМПОРТ
 
 # basedir нужен для построения абсолютных путей
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,11 +10,12 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    # ===== НОВАЯ НАСТРОЙКА ДЛЯ "ЗАПОМНИТЬ МЕНЯ" =====
+    PERMANENT_SESSION_LIFETIME = timedelta(days=30)
+
     API_HOST = os.environ.get('API_HOST') or 'v3.football.api-sports.io'
     API_KEY = os.environ.get('API_KEY') or '20244b21779b98fc5ea2bdcd18680816'
 
-    # ----- НАЧАЛО НОВОГО КОДА -----
     # Flask-Uploads ищет эту переменную, чтобы знать, куда сохранять файлы.
     # Имя переменной должно быть UPLOADED_{ИМЯ СЕТА}_DEST. У нас сет называется 'avatars'.
     UPLOADED_AVATARS_DEST = os.path.join(basedir, 'app', 'static', 'uploads', 'avatars')
-    # ----- КОНЕЦ НОВОГО КОДА -----
